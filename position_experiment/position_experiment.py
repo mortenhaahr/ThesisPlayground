bb36a942f8b89122f4117d8141dbcef9e5d874ce
Morten Haahr Kristensen <mortenhaahrkristensen@gmail.com>
2023-09-05 13:19:11 +0200

    Now with velocity

--- position_experiment/position_experiment.py
+++ position_experiment/position_experiment.py
@@ -7,14 +7,7 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 DEVICE = "cpu"
-
-
-def init_tensors(module):
-    # Init the tensors with random values based on a xavier uniform distribution
-    for m in module.modules():
-        if type(m) == torch.nn.Linear:
-            # NOTE: Weights are technically already initialized - so xavier is not needed
-            torch.nn.init.xavier_uniform_(m.weight)
+MODEL = "vanilla"
 
 
 # plot loss functions as function of training steps
@@ -45,11 +38,9 @@ def plot_sample_points(t_vals, sample_points, ax):
     )
 
 
-def make_nn():
+def make_nn(N_inputs, N_outputs):
     hidden_dim = 32
     hidden_layers = 5
-    N_inputs = 1
-    N_outputs = 1
 
     # Create input layer with `N_inputs` and `hidden_dim` outputs. Apply softplus.
     layers = [torch.nn.Linear(N_inputs, hidden_dim), torch.nn.Softplus()]
@@ -57,37 +48,39 @@ def make_nn():
         layers.extend([torch.nn.Linear(hidden_dim, hidden_dim), torch.nn.Softplus()])
     layers.append(torch.nn.Linear(hidden_dim, N_outputs))
     net = torch.nn.Sequential(*layers).double().to(DEVICE)
-    init_tensors(net)
     return net
 
 
 def main():
-    y0 = [0, 1]  # Start at 0, driving with 1 m/s
+    y0 = [0, 0, 0.001]  # Start at 0, driving with 0 m/s, accelerating with 1 mm/s²
     step_size = 10.0  # [s]
     t_start = 0
     t_end = 2000
     t_eval = np.arange(t_start, t_end, step_size)
-    subsample_every = 20
+    subsample_every = 10
     N_epochs = 2000
 
-    y_true = [y0[0] + t * y0[1] for t in t_eval]
+    # Formula for position and velocity
+    pos = lambda t: y0[0] + t * y0[1] + 1 / 2 * y0[2] * t**2
+    vel = lambda t: y0[2] * t
+
+    y_true = np.array([[pos(t) for t in t_eval], [vel(t) for t in t_eval]])
 
     t = torch.tensor(t_eval, device="cpu", requires_grad=True)
-    y_train = torch.tensor(y_true[::subsample_every]).to(DEVICE)
+    y_train = torch.tensor(y_true[:, ::subsample_every]).to(DEVICE)
     t_train = torch.tensor(t_eval[::subsample_every], requires_grad=True).to(DEVICE)
 
-    nn = make_nn()
-    optim = torch.optim.Adam(nn.parameters())
     losses = []
-
-    for epoch in tqdm(range(N_epochs), desc="vanilla: training epoch"):
-        out = nn(t_train.unsqueeze(-1)).T
-        y_train_v = y_train.view(1, len(y_train))
-        loss_collocation = torch.nn.functional.mse_loss(out, y_train_v)
-        loss_collocation.backward()
-        optim.step()
-        nn.zero_grad()
-        losses.append(loss_collocation.item())
+    if MODEL == "vanilla":
+        nn = make_nn(1, 2)
+        optim = torch.optim.Adam(nn.parameters())
+        for epoch in tqdm(range(N_epochs), desc="vanilla: training epoch"):
+            out = nn(t_train.unsqueeze(-1)).T
+            loss_collocation = torch.nn.functional.mse_loss(out, y_train)
+            loss_collocation.backward()
+            optim.step()
+            nn.zero_grad()
+            losses.append(loss_collocation.item())
 
     y_pred = (
         nn(t.unsqueeze(-1)).detach().cpu().T
@@ -97,15 +90,16 @@ def main():
 
     fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
     fig.canvas.manager.set_window_title("Vanilla model")
-    plot_prediction(t_eval, y_pred[0], y_true, "x(t)", ax1, "b")
-    # plot_prediction(y_pred[1], y_true[1], "ω(t)", ax2, "r")
+    plot_prediction(t_eval, y_pred[0], y_true[0], "x(t)", ax1, "b")
+    plot_prediction(t_eval, y_pred[1], y_true[1], "v(t)", ax2, "r")
 
     t_samples = t_train.detach().cpu()
-    x_samples = y_true[::subsample_every]
+    x_samples = y_true[:, ::subsample_every][0]
+    v_samples = y_true[:, ::subsample_every][1]
     plot_sample_points(t_samples, x_samples, ax1)
-    # plot_sample_points(t_samples, omega_samples, ax2)
+    plot_sample_points(t_samples, v_samples, ax2)
 
-    ax2.legend()
+    ax1.legend()
     plt.tight_layout()
 
     plot_losses("Loss vanilla", losses)
